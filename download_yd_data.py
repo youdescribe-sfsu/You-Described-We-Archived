2526fce3ac33b7897e03112c8af3f548faf2fbe1
Benjamin Kao <benjamin.kao00@gmail.com>
2022-10-27 11:54:12 -0700

    Added dataset

--- download_yd_data.py
+++ download_yd_data.py
@@ -13,12 +13,16 @@ def parseArgs():
     parser = argparse.ArgumentParser(description="This program downloads data from the YouDescribe data repository for use in AI/ML applications.")
     parser.add_argument('--inputFile', help="The input file that contains all of the audio clips. This should be a CSV file with at least these columns: [videos_youtube_id, audio_descriptions_id, audio_clips_id, file_path, file_name]")
     parser.add_argument('--audioDescDir', help="The path to the directory where the downloaded audio clips will be stored. The downloaded audio clips will be separated into different folders by YouTube video ID and Audio Description ID. Should end with a '/'")
+    parser.add_argument('--useAvailableOnly', default=True, help=("Choose whether or not you want to download the audio clips of YouTube videos that are no longer available."
+     "Default is set to only download audio clips from available YouTube videos."
+     "DISCLAIMER: We do not keep track of the status of all of the YouTube videos that we provide audio descriptions for in real time."
+    " Therefore, we cannot guarantee that there are audio clips that say they are from an 'available' YouTube video but the YouTube video was taken down."))
 
     args = parser.parse_args()
 
     return args
 
-def downloadAudioDescriptions(inputFile, audioDescDir):
+def downloadAudioDescriptions(inputFile, audioDescDir, useAvailableOnly):
     
     if inputFile == None or len(inputFile) <= 0:
         print(f"Error: No input file given")
@@ -28,13 +32,14 @@ def downloadAudioDescriptions(inputFile, audioDescDir):
         print(f"Error: No audioDescDir given")
         return
 
-    with open(inputFile, "r") as f:
-        reader = csv.reader(f)
+    with open(inputFile, "r", encoding='utf-8') as f:
+        reader = csv.reader(f, dialect=csv.excel)
 
         header = next(reader)
         inputRows = [row for row in reader]
 
     videoIDCol = -1
+    videoStatusCol = -1
     audioDescIDCol = -1
     audioClipsIDCol = -1
     filePathCol = -1
@@ -45,6 +50,8 @@ def downloadAudioDescriptions(inputFile, audioDescDir):
 
         if headingLower.__contains__('videos_youtube_id'):
             videoIDCol = col
+        elif headingLower.__contains__('videos_youtube_status'):
+            videoStatusCol = col
         elif headingLower.__contains__('audio_descriptions_id'):
             audioDescIDCol = col
         elif headingLower.__contains__('audio_clips_id'):
@@ -59,14 +66,21 @@ def downloadAudioDescriptions(inputFile, audioDescDir):
         print(f"Error: Input csv files does not contain columns named 'videos_youtube_id', 'audio_descriptions_id', 'audio_clips_id', 'file_path', and 'file_name'. Aborting...")
         return
     
+    if videoStatusCol == -1:
+        print(f"WARNING: No 'videos_youtube_status' column. Will assume to download all audio clips regardless of video status.")
+    
     print(f"Input file format has been validated. Starting download.")
 
     for col, row in enumerate(inputRows):
         videoID = row[videoIDCol]
+        videoStatus = row[videoStatusCol]
         audioDescID = row[audioDescIDCol]
         audioClipID = row[audioClipsIDCol]
         filePath = row[filePathCol]
         fileName = row[fileNameCol]
+
+        if useAvailableOnly and videoStatus.lower() == 'unavailable':
+            continue
 
         localFilePathDir = f"{audioDescDir}{videoID}/{audioDescID}"
         localFilePath = f"{localFilePathDir}/{fileName}"
@@ -87,7 +101,7 @@ def downloadAudioDescriptions(inputFile, audioDescDir):
 
         doc = requests.get(f"http://13.58.32.80{filePath}/{fileName}")
 
-        if doc.status_code >=200 and doc.status_code < 300:
+        if not (doc.status_code >=200 and doc.status_code < 300):
             print(f"Please download the latest version of this data repository to get the latest version of the download script.")
 
         with open(localFilePath, 'wb') as f:
@@ -98,7 +112,7 @@ def downloadAudioDescriptions(inputFile, audioDescDir):
 def main():
     opt = parseArgs()
 
-    downloadAudioDescriptions(opt.inputFile, opt.audioDescDir)
+    downloadAudioDescriptions(opt.inputFile, opt.audioDescDir, opt.useAvailableOnly)
 
 
 
